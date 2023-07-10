3070543a8f39381cc0d65a1f0322d0e7c136ec45
Bladgroen <67104310+Bladgroen@users.noreply.github.com>
2023-07-10 20:18:33 +0200

    💄added materialUI

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_APP_BASE_API: string;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elementName: string]: any;
+  }
+}
