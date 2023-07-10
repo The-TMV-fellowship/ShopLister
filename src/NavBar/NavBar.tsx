fa180bd44c7f83e7b55b72c6d19b3f605a878fee
Bladgroen <67104310+Bladgroen@users.noreply.github.com>
2023-07-10 22:32:44 +0200

    💄added nav bar

---
+++ src/NavBar/NavBar.tsx
@@ -0,0 +1,34 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import RestoreIcon from "@mui/icons-material/Restore";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Paper from "@mui/material/Paper";
+import { useState } from "react";
+function NavBar() {
+  const [value, setValue] = useState<number>(0);
+  return (
+    <>
+      <div>
+        <Paper
+          sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+          elevation={3}
+        >
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue: number) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          </BottomNavigation>
+        </Paper>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
