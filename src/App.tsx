4f0e825f220815673e82feca46efe478fd7f291f
Bladgroen <67104310+Bladgroen@users.noreply.github.com>
2023-07-30 20:29:10 +0200

    🎨moved App code to HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./Header/Header";
-import ShoppingListCard from "./HomePage/ShoppingListCard";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <ShoppingListCard isGroup={true} checkedOffAmount={5} listSize={5} />
-      <ShoppingListCard isGroup={false} checkedOffAmount={2} listSize={4} />
-      <ShoppingListCard isGroup={true} checkedOffAmount={7} listSize={10} />
-      <ShoppingListCard isGroup={true} checkedOffAmount={1} listSize={5} />
-      <ShoppingListCard isGroup={false} checkedOffAmount={11} listSize={20} />
+
     </>
   );
 }
