88572723fb98491e42006aed167dd91d75af6fe6
Bladgroen <67104310+Bladgroen@users.noreply.github.com>
2023-07-30 20:36:30 +0200

    ✨added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./Header/Header";
+import HomePage from "./HomePage/HomePage";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header></Header>
-
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
