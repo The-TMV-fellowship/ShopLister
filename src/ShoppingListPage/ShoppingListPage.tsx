263e2c0862f14eb542ffd1cf50ddfaebd15ab6e4
tim-rooman <timrulezzz12@gmail.com>
2023-08-05 15:10:44 +0200

    ✨ Tracking amount of checked boxes

--- src/ShoppingListPage/ShoppingListPage.tsx
+++ src/ShoppingListPage/ShoppingListPage.tsx
@@ -1,9 +1,14 @@
 import ShoppingListItem from "./ShoppingListItem"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function ShoppingListPage() {
 
   const [checkboxStatus, setCheckboxStatus] = useState({});
+  const [percentageChecked, setPercentageChecked] = useState<number>(0);
+
+  useEffect(() => {
+    calculateCheckedBoxAmount();
+  }, [checkboxStatus]);
 
   const handleCheckboxChange = (childId, isChecked) => {
     setCheckboxStatus((prevStatus) => ({
@@ -12,12 +17,22 @@ export default function ShoppingListPage() {
     }));
   };
 
+  const calculateCheckedBoxAmount = () => {
+    let amountChecked:number = 0;
+    for (const [key, value] of Object.entries(checkboxStatus)) {
+      if(value === true){
+        amountChecked++;
+      }
+    }
+     setPercentageChecked(Math.round((amountChecked/ Object.keys(checkboxStatus).length) * 100));
+  }
+
   return (
     <div>
       <ShoppingListItem childId="child1" isChecked={checkboxStatus['child1']} onCheckboxChange={handleCheckboxChange} itemName={"Brood"}/>
       <ShoppingListItem childId="child2" isChecked={checkboxStatus['child2']} onCheckboxChange={handleCheckboxChange} itemName={"Coca-cola"}/>
       <ShoppingListItem childId="child3" isChecked={checkboxStatus['child3']} onCheckboxChange={handleCheckboxChange} itemName={"Eieren"}/>
-      <p>Checkbox is {checkboxStatus["child1"]  ? 'checked' : 'unchecked'}</p>
+      <p>Checkboxes are {percentageChecked} percent checked</p>
     </div>
   );
 }
