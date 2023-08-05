3f44a422ec7fbfe2b2aa0f2a110a66f9b0d8049d
tim-rooman <timrulezzz12@gmail.com>
2023-08-05 14:49:59 +0200

    ✨ ListItems now take props for data

--- src/ShoppingListPage/ShoppingListItem.tsx
+++ src/ShoppingListPage/ShoppingListItem.tsx
@@ -1,12 +1,16 @@
 import "./ShoppingListPage.scss"
 
-export default function ShoppingListPage() {
+export default function ShoppingListPage({ childId, isChecked, onCheckboxChange, itemName }) {
+  const handleChange = (event) => {
+    const newCheckedValue = event.target.checked;
+    onCheckboxChange(childId, newCheckedValue);
+  };
 
   return (
       <div className="shoppinglistitem">
         <div className="shoppinglistitem__lefthalf">
-        <input type="checkbox"></input>
-        <p>Item name</p>
+        <input type="checkbox"  checked={isChecked} onChange={handleChange}></input>
+        <p>{itemName}</p>
         </div>
        
         <p>Category Icon</p>
--- src/ShoppingListPage/ShoppingListPage.tsx
+++ src/ShoppingListPage/ShoppingListPage.tsx
@@ -1,12 +1,23 @@
 import ShoppingListItem from "./ShoppingListItem"
+import { useState } from "react";
 
 export default function ShoppingListPage() {
 
+  const [checkboxStatus, setCheckboxStatus] = useState({});
+
+  const handleCheckboxChange = (childId, isChecked) => {
+    setCheckboxStatus((prevStatus) => ({
+      ...prevStatus,
+      [childId]: isChecked,
+    }));
+  };
+
   return (
     <div>
-      <ShoppingListItem/>
-      <ShoppingListItem/>
-      <ShoppingListItem/>
+      <ShoppingListItem childId="child1" isChecked={checkboxStatus['child1']} onCheckboxChange={handleCheckboxChange} itemName={"Brood"}/>
+      <ShoppingListItem childId="child2" isChecked={checkboxStatus['child2']} onCheckboxChange={handleCheckboxChange} itemName={"Coca-cola"}/>
+      <ShoppingListItem childId="child3" isChecked={checkboxStatus['child3']} onCheckboxChange={handleCheckboxChange} itemName={"Eieren"}/>
+      <p>Checkbox is {checkboxStatus["child1"]  ? 'checked' : 'unchecked'}</p>
     </div>
   );
 }
