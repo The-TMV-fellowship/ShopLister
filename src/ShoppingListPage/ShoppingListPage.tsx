15f997f08e9e682a59daccb4a4250ff95e225c72
tim-rooman <timrulezzz12@gmail.com>
2023-08-05 15:18:08 +0200

    ✨ Adding progressbar

--- src/ShoppingListPage/ShoppingListPage.tsx
+++ src/ShoppingListPage/ShoppingListPage.tsx
@@ -1,10 +1,31 @@
-import ShoppingListItem from "./ShoppingListItem"
 import { useState, useEffect } from "react";
+import ShoppingListItem from "./ShoppingListItem";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import { styled } from "@mui/material/styles";
 
 export default function ShoppingListPage() {
 
   const [checkboxStatus, setCheckboxStatus] = useState({});
   const [percentageChecked, setPercentageChecked] = useState<number>(0);
+
+  const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 10,
+    borderRadius: 5,
+    width: "100%",
+    flexShrink: 0,
+    padding: 0,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      borderRadius: 5,
+      backgroundColor: theme.palette.mode === "light" ? "green" : "#308fe8",
+    },
+  }));
+  
 
   useEffect(() => {
     calculateCheckedBoxAmount();
@@ -25,10 +46,12 @@ export default function ShoppingListPage() {
       }
     }
      setPercentageChecked(Math.round((amountChecked/ Object.keys(checkboxStatus).length) * 100));
+     console.log(checkboxStatus);
   }
 
   return (
     <div>
+       <BorderLinearProgress variant="determinate" value={percentageChecked? percentageChecked : 0}/>
       <ShoppingListItem childId="child1" isChecked={checkboxStatus['child1']} onCheckboxChange={handleCheckboxChange} itemName={"Brood"}/>
       <ShoppingListItem childId="child2" isChecked={checkboxStatus['child2']} onCheckboxChange={handleCheckboxChange} itemName={"Coca-cola"}/>
       <ShoppingListItem childId="child3" isChecked={checkboxStatus['child3']} onCheckboxChange={handleCheckboxChange} itemName={"Eieren"}/>
