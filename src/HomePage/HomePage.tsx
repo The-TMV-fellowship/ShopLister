bfb5eef8cd7f45ecc67d069623e2c89ea67d0043
tim-rooman <timrulezzz12@gmail.com>
2023-08-19 12:11:04 +0200

    ✨ Added WIP header

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,9 +1,14 @@
 import ShoppingListCard from "./ShoppingListCard";
 import './HomePage.scss';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function HomePage() {
   return (
     <>
+        <div className="homepageheader">
+          <MenuIcon/> <span className="homepageheader__pagedescription">Your shoppinglists</span>
+        </div>
+        
       <ShoppingListCard isGroup={true} checkedOffAmount={5} listSize={5} />
       <ShoppingListCard isGroup={false} checkedOffAmount={2} listSize={4} />
       <ShoppingListCard isGroup={true} checkedOffAmount={7} listSize={10} />
