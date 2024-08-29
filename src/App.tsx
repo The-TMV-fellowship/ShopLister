d6cb89636de0d9b9df7e83039fffda853403f1dd
tim-rooman <timrooman@gmail.com>
2024-08-29 17:06:34 +0200

    💄 Adding global colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AddList from "./AddList/AddList";
 import "./App.scss";
 import Header from "./Header/Header";
-import HomePage from "./HomePage/HomePage";
 import LoginPage from "./Login/LoginPage";
+//import HomePage from "./NewDesign/HomePage/HomePage";
+import WelcomePage from "./NewDesign/WelcomePage/WelcomePage";
 import NotFound from "./NotFound/NotFound";
 import RegisterPage from "./Register/RegisterPage";
 import ShoppingListPage from "./ShoppingListPage/ShoppingListPage";
-import WelcomePage from "./WelcomePage/WelcomePage";
 
 function App() {
   return (
     <Router>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<WelcomePage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/welcome" element={<WelcomePage />} />
