f73f8628ebd3be97fa1c2fb87a74a64b63b44481
tim-rooman <timrooman@gmail.com>
2024-08-31 11:23:32 +0200

    ✨ Added new login page to the router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AddList from "./AddList/AddList";
 import "./App.scss";
 import Header from "./Header/Header";
-import LoginPage from "./Login/LoginPage";
+import LoginPage from "./NewDesign/LoginPage/LoginPage";
 //import HomePage from "./NewDesign/HomePage/HomePage";
 import WelcomePage from "./NewDesign/WelcomePage/WelcomePage";
 import NotFound from "./NotFound/NotFound";
