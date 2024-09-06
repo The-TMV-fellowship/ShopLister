66193b96b800557d052bc212709f55d7a3a4914c
tim-rooman <timrooman@gmail.com>
2024-09-06 20:27:12 +0200

    💄 Updated LoginPage styles

--- src/NewDesign/LoginPage/LoginPage.tsx
+++ src/NewDesign/LoginPage/LoginPage.tsx
@@ -1,4 +1,10 @@
 import CircularProgress from "@mui/material/CircularProgress";
+/*
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+*/
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -7,6 +13,7 @@ import {
   validateForm,
   validationSchema,
 } from "../../Login/LoginPageLogic";
+import GeneralWave from "../../assets/generalWave.svg";
 import { LoginFormData } from "../../interfaces/types";
 import "./LoginPage.scss";
 
@@ -38,59 +45,63 @@ export default function LoginPage() {
 
   return (
     <div>
-      <h1>Log in</h1>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-        validateOnChange={false}
-        validateOnBlur={false}
-      >
-        <Form className="form">
-          <div className="loginFormField">
-            <label htmlFor="username" className="loginFormField__label">
-              Username
-            </label>
-            <Field
-              type="username"
-              name="username"
-              className="loginFormField__field"
-            />
-            <ErrorMessage
-              name="username"
-              component="div"
-              className="error-message"
-            />
-          </div>
+      <img src={GeneralWave}></img>
+      <div className="subContainer">
+        <h1>Log in</h1>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+          validateOnChange={false}
+          validateOnBlur={false}
+        >
+          <Form>
+            <div className="formInput">
+              <label htmlFor="username">Username</label>
+              <Field type="username" name="username" />
+              <ErrorMessage
+                name="username"
+                component="div"
+                className="error-message"
+              />
+            </div>
 
-          <div className="loginFormField">
-            <label htmlFor="password" className="loginFormField__label">
-              Password
-            </label>
-            <Field
-              type="password"
-              name="password"
-              className="loginFormField__field"
-            />
-            <ErrorMessage
-              name="password"
-              component="div"
-              className="error-message"
-            />
-          </div>
+            <div className="formInput">
+              <label htmlFor="password">Password</label>
+              <Field type="password" name="password" />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="error-message"
+              />
+            </div>
 
-          <button type="submit" disabled={loading}>
-            {loading ? (
-              <CircularProgress size={20} color="inherit" />
-            ) : (
-              "Submit"
-            )}
-          </button>
-        </Form>
-      </Formik>
-      <p>
-        Don't have an account? <a>Sign up</a>
-      </p>
+            <div className="belowFormText">
+              <div>
+                <input
+                  type="checkbox"
+                  id="rememberPass"
+                  name="rememberPass"
+                  className="checkbox"
+                />
+                <label htmlFor="rememberPass">Remember me</label>
+              </div>
+              <a href="/">Forgot password?</a>
+            </div>
+
+            <button type="submit" disabled={loading}>
+              {loading ? (
+                <CircularProgress size={20} color="inherit" />
+              ) : (
+                "Log in"
+              )}
+            </button>
+          </Form>
+        </Formik>
+        <p>
+          Don't have an account? <a>Sign up</a>
+        </p>
+      </div>
     </div>
   );
 }
