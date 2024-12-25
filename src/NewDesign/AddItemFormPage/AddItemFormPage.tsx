51496318b92b881aa65284d75dfc96b37ca1e50c
Tim Rooman <timrooman@gmail.com>
2024-12-25 16:46:57 +0100

    💄 Setting up basic styles for additempage

--- src/NewDesign/AddItemFormPage/AddItemFormPage.tsx
+++ src/NewDesign/AddItemFormPage/AddItemFormPage.tsx
@@ -1,21 +1,35 @@
 import { useNavigate } from "react-router-dom";
 import GeneralWave from "../../assets/generalWave.svg";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import "./AddItemFormPage.scss";
 
 export default function AddItemFormPage() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const navigateBackToList = () =>{
-  navigate("/shoppinglist")
-}
-
+  const navigateBackToList = () => {
+    navigate("/shoppinglist");
+  };
 
   return (
     <div>
       <img src={GeneralWave}></img>
-      <h1>New Item</h1>
-      <span>Itemname input field placeholder</span>
-      <button>+ Add item</button>
-      <a onClick={() => navigateBackToList()}>Cancel</a>
+      <div className="subcontainer">
+        <h1>New Item</h1>
+        <div className="formInput">
+          <div className="inputFieldContainer">
+            <ListAltIcon className="inputIconAccent" />
+            <div className="seperatorLine"></div>
+            <input
+              type="text"
+              name="listname"
+              placeholder="name your item"
+              className="inputField"
+            />
+          </div>
+        </div>
+        <button>+ Add item</button>
+        <a onClick={() => navigateBackToList()}>Cancel</a>
+      </div>
     </div>
   );
 }
