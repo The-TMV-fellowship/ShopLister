9d070b821af60c36eab01f180d40c18501e0ff9c
Tim Rooman <timrooman@gmail.com>
2025-02-06 18:42:16 +0100

    ✨ Added authentication checker

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,48 @@ import WelcomePage from "./NewDesign/WelcomePage/WelcomePage";
 import NotFound from "./NotFound/NotFound";
 import ShoppingListPage from "./NewDesign/ListDetailPage/ListDetailPage";
 import AddItemFormPage from "./NewDesign/AddItemFormPage/AddItemFormPage";
+import RequireAuth from "./RequireAuth";
 
 function App() {
   return (
     <Router>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <HomePage />
+            </RequireAuth>
+          }
+        />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/welcome" element={<WelcomePage />} />
-        <Route path="/shoppinglist" element={<ShoppingListPage />} />
-        <Route path="/addlist" element={<AddListFormPage />} />
-        <Route path="/addlistitem" element={<AddItemFormPage/>}/>
+        <Route
+          path="/shoppinglist"
+          element={
+            <RequireAuth>
+              <ShoppingListPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/addlist"
+          element={
+            <RequireAuth>
+              <AddListFormPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/addlistitem"
+          element={
+            <RequireAuth>
+              <AddItemFormPage />
+            </RequireAuth>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
